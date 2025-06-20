7dc97b3b431ead6565ee55bf81f035bb733b4f61
Aitor Z. <75847372+aitor74@users.noreply.github.com>
2025-06-20 10:22:31 +0200

    Update Get-OSInfo.ps1

--- Scripts/Get-OSInfo.ps1
+++ Scripts/Get-OSInfo.ps1
@@ -2,6 +2,12 @@
 # Muestra la versión y nombre del sistema operativo.
 
 $os = Get-CimInstance Win32_OperatingSystem
+
+Write-Host "----------------------------------------"
 Write-Host "Sistema operativo: $($os.Caption)"
 Write-Host "Versión: $($os.Version)"
 Write-Host "Arquitectura: $($os.OSArchitecture)"
+Write-Host "----------------------------------------"
+
+Write-Host ""
+Read-Host "Presiona [Enter] para cerrar"
